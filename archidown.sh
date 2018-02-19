4feae4c638f0c5fcc68abab056d85750f11f5cdd
librenauta <librenauta@partidopirata.com.ar>
2018-02-19 02:12:55 -0300

    Update archidown.sh

--- archidown.sh
+++ archidown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-i=5000
-while [ $i -lt 50000 ]; do
+i=100000
+while [ $i -lt 1000 ]; do
     i=$[$i + 1];
     wget -O - http://archillect.com/$i | grep -E '<img id="ii" src=.*/>' | sed -e 's/<img id="ii" src=//' -e 's/\/>//' -e 's/^[[:space:]]*//' >> links.txt
 done
